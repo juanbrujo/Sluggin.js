8773cb31c385f7e767a94f46c982703b333c16ee
Jorge Epuñan <juanbrujo@gmail.com>
2015-06-15 17:16:40 -0400

    v0.0.4

--- dist/Sluggin.js
+++ dist/Sluggin.js
@@ -1,5 +1,5 @@
 /*
- *  Sluggin.js - v0.0.1
+ *  Sluggin.js - v0.0.4
  *  Converts string into websafe-friendly-clean piece of text
  *
  *  https://github.com/juanbrujo/Sluggin.js
@@ -9,11 +9,9 @@
  *  License: MIT
  *  ©2015
  */
-var Sluggin = (function Sluggin(global) {
+var Sluggin = (function Sluggin() {
   
   "use strict";
-  
-  var self = this;
   
   var characters = [
     "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
@@ -22,7 +20,7 @@ var Sluggin = (function Sluggin(global) {
     "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ь", "Ю", "Я",
     "Ї", "ї", "Є", "є", "Ы", "ы", "Ё", "ё",
     "ı", "İ", "ğ", "Ğ", "ü", "Ü", "ş", "Ş", "ö", "Ö", "ç", "Ç",
-    "Á", "á", "Â", "â", "Ã", "ã", "À", "à", "Ç", "ç", "É", "é", "Ê", "ê", "È", "è", "Í", 
+    "Á", "á", "Â", "â", "Ã", "ã", "À", "à", "Ç", "ç", "É", "é", "Ê", "ê", "È", "è", "Ä", "ä", "Í", 
     "í", "Ó", "ó", "Ô", "ô", "Õ", "õ", "Ú", "ú", "Ñ", "ñ", "ą", "ć", "ę", "ł", "ń", "ó", "ś", "ź", "ż", "Ą", "Ć", "Ę", "Ł", "Ń", "Ó", "Ś", "Ź", "Ż"
   ];
 
@@ -33,7 +31,7 @@ var Sluggin = (function Sluggin(global) {
     "P", "R", "S", "T", "U", "F", "H", "Ts", "Ch", "Sh", "Sht", "A", "Y", "Yu", "Ya",
     "I", "i", "Ye", "ye", "I", "i", "Yo", "yo",
     "i", "I", "g", "G", "u", "U", "s", "S", "o", "O", "c", "C",
-    "A", "a", "A", "a", "A", "a", "A", "a", "C", "c", "E", "e", "E", "e", "E", "e", "I",
+    "A", "a", "A", "a", "A", "a", "A", "a", "C", "c", "E", "e", "E", "e", "E", "e", "A", "a", "I",
     "i", "O", "o", "O", "o", "O", "o", "U", "u", "N", "n", "a", "c", "e", "l", "n", "o", "s", "z", "z", "A", "C", "E", "L", "N", "O", "S", "Z", "Z"
   ];
 
@@ -45,7 +43,7 @@ var Sluggin = (function Sluggin(global) {
   }
 
   function str_replace(search, replace, subject, count) {
-    var i = 0, j = 0, temp = '', repl = '', sl = 0, fl = 0,
+    var i = 0, j = 0, temp = "", repl = "", sl = 0, fl = 0,
         f = [].concat(search),
         r = [].concat(replace),
         s = subject,
@@ -56,12 +54,12 @@ var Sluggin = (function Sluggin(global) {
     }
 
     for (i=0, sl=s.length; i < sl; i++) {
-      if (s[i] === '') {
+      if (s[i] === "") {
         continue;
       }
       for (j=0, fl=f.length; j < fl; j++) {
-        temp = s[i]+'';
-        repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0];
+        temp = s[i] + "";
+        repl = ra ? (r[j] !== undefined ? r[j] : "") : r[0];
         s[i] = (temp).split(f[j]).join(repl);
         if (count && s[i] !== temp) {
           window[count] += (temp.length-s[i].length)/f[j].length;}
@@ -70,9 +68,9 @@ var Sluggin = (function Sluggin(global) {
     return sa ? s : s[0];
   }
   
-  function SlugginOutput(text) {
+  function Sluggin(text) {
     
-    if( typeof text === 'string' ){
+    if( typeof text === "string" ){
     
       var output = convert(text)
       .toLowerCase()
@@ -81,7 +79,7 @@ var Sluggin = (function Sluggin(global) {
       .replace(/[^a-z\u0400-\u04FF0-9-]+/g, "")
       .replace(/[-]+/g, "-")
       .replace(/^-+|-+$/g, "")
-      .replace(/[-]+/g, '-');
+      .replace(/[-]+/g, "-");
       
       return output;
 
@@ -89,14 +87,19 @@ var Sluggin = (function Sluggin(global) {
     
   }
 
-  return SlugginOutput;
+  return Sluggin;
 
 })(this);
 
-
-
-// var texto = Sluggin( document.getElementById("hola") );
- var texto = Sluggin( "hola ke àse" );
-
-console.log( texto );
+if (typeof module !== "undefined" && module.exports) {
+  // export functions for use in Node
+  module.exports.Sluggin = function(text) {
+    return Sluggin(text);
+  };
+} else if (typeof define !== "undefined" && define.amd) {
+  // export function for use in AMD
+  define([], function (text) {
+    return Sluggin(text);
+  });
+} 
 
