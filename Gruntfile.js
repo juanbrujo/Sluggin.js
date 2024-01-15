1212f7a3f6dcf8033af1e7336e47be7192689ae7
Jorge Epuñan <juanbrujo@gmail.com>
2024-01-15 10:20:11 -0300

    docs: better docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
-
 	grunt.registerTask("default", ["uglify"]);
 	grunt.registerTask("testjs", ["jshint"]);
 };
